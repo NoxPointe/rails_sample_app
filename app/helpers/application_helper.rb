2d701b08c962914cc18a685fac4c46b0367733a7
Pat East <pat@pixotech.com>
2016-09-11 16:32:27 -0500

    Add a full_title helped for page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	#returns the full title on a page by page basis
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		return page_title.empty? ? base_title : page_title + " | " + base_title
+	end
 end
